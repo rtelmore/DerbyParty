2a203794069da87606d583a557aea33b64a5fe06
Ryan Elmore <Ryan.Elmore@nrel.gov>
2014-03-18 18:34:38 -0600

    need to fix some shit

--- src/functions.R
+++ src/functions.R
@@ -2,12 +2,16 @@
 ## Date: 18 Mar 2014
 ## Include project-specific functions in this file
 
-FinalPayout <- function(bet.df, odds, results){
+FinalWinPayout <- function(bet.df, bet.type = "win", results){
   kBets <- length(bet.df$horse)
+  bet.types <- 1:3
+  names(bet.types) <- c("win", "place", "show")
+  kFactor <- bet.types[bet.type]
+  odds <- FinalOdds(bet.df, kFactor = kFactor)
   win.payouts <- rep(0, kBets)
   for (i in 1:kBets) {
-    if(bet.df$horse[i] == results[1]) {
-      win.payouts[i] <- bet.df$amount[i]*odds$odds[odds$horse == results[1]] +
+    if(bet.df$horse[i] %in% results) {
+      win.payouts[i] <- bet.df$amount[i]*odds$odds[odds$horses == results[1]] +
                         bet.df$amount[i]
     }
   }
@@ -15,14 +19,27 @@ FinalPayout <- function(bet.df, odds, results){
   return(bet.df)
 }
 
+FinalPlacePayout <- function(bet.df, odds. results) {
+  kBets <- length(bet.df$horse)
+  win.payouts <- rep(0, kBets)
+  for (i in 1:kBets) {
+    if(bet.df$horse[i] %in% results[1:2]) {
+      win.payouts[i] <- bet.df$amount[i]*odds$odds[odds$horse == results[1]] +
+                        bet.df$amount[i]
+    }
+  }
+  bet.df$payouts <- win.payouts
+  return(bet.df)
+  
+}
 
-FinalOdds <- function(bet.df){
+FinalOdds <- function(bet.df, kFactor){
   horses <- unique(bets.df$horse)
   bet <- 1:length(horses)
   for (horse in bet){
     bet[horse] <- sum(bets.df$amount[bets.df$horse == horses[horse]])
   }
-  odds <- data.frame(horse = horses, amount.bet = bet, 
-                     odds = (sum(bets.df$amount) - bet)/bet)
-  return(odds)
+  odds <- (sum(bets.df$amount)/kFactor - bet)/bet
+  return(data.frame(horses = horses, total = bet, odds = odds))
 }
+
