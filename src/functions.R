9e383d517c983a6993cf5ac064b3a0425c149ab0
Ryan Elmore <Ryan.Elmore@nrel.gov>
2014-03-18 18:48:03 -0600

    final commit on flight to SLC -- done!

--- src/functions.R
+++ src/functions.R
@@ -2,7 +2,7 @@
 ## Date: 18 Mar 2014
 ## Include project-specific functions in this file
 
-FinalWinPayout <- function(bet.df, bet.type = "win", results){
+FinalPayout <- function(bet.df, bet.type = "win", results){
   kBets <- length(bet.df$horse)
   bet.types <- 1:3
   names(bet.types) <- c("win", "place", "show")
@@ -10,27 +10,13 @@ FinalWinPayout <- function(bet.df, bet.type = "win", results){
   odds <- FinalOdds(bet.df, kFactor = kFactor)
   win.payouts <- rep(0, kBets)
   for (i in 1:kBets) {
-    if(bet.df$horse[i] %in% results) {
-      win.payouts[i] <- bet.df$amount[i]*odds$odds[odds$horses == results[1]] +
-                        bet.df$amount[i]
+    if(bet.df$horse[i] %in% results[1:kFactor]) {
+      multiplier <- odds$odds[odds$horses == bet.df$horse[i]]
+      win.payouts[i] <- bet.df$amount[i]*(multiplier + 1)
     }
   }
   bet.df$payouts <- win.payouts
   return(bet.df)
-}
-
-FinalPlacePayout <- function(bet.df, odds. results) {
-  kBets <- length(bet.df$horse)
-  win.payouts <- rep(0, kBets)
-  for (i in 1:kBets) {
-    if(bet.df$horse[i] %in% results[1:2]) {
-      win.payouts[i] <- bet.df$amount[i]*odds$odds[odds$horse == results[1]] +
-                        bet.df$amount[i]
-    }
-  }
-  bet.df$payouts <- win.payouts
-  return(bet.df)
-  
 }
 
 FinalOdds <- function(bet.df, kFactor){
