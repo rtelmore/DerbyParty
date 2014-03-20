ab5a4ee9ef53478fa4c3026fc6fea008a0adaa16
Ryan Elmore <Ryan.Elmore@nrel.gov>
2014-03-19 18:44:34 -0600

    fuck, broke it

--- src/do.R
+++ src/do.R
@@ -8,11 +8,23 @@
 source(paste(.project.path, "src/load.R", sep=""))
 
 set.seed(298)
-bets.df <- data.frame(bettor = LETTERS[1:10], 
-                      amount = sample(c(1, 5), size = 10, rep = T),
-                      horse = sample(letters[1:5], size = 10, rep = T))
+kBets <- 50
+bets.df <- data.frame(bettor = sample(c(letters, LETTERS), 
+                                      size = kBets, 
+                                      rep = F), 
+                      amount = sample(c(1, 5), size = kBets, rep = T),
+                      horse = sample(letters[1:5], size = kBets, rep = T),#)
+                      type = sample(c("win", "place", "show"), 
+                                    size = kBets, rep = T))
 
 results <- c("a", "b", "e")
+bets <- bets.df[bets.df$type == "win", ]
+ss <- FinalPayoutOld(bets, bet.type = "win", results)
+
+bets.df.2 <- cbind(bets.df, type = rep("show", kBets))
+gg <- FinalPayout(bets.df, bet.type = "show", results)
+
+tt <- bets.df[bets.df$type == "win", ]
 
 total.pool <- sum(bets.df$amount)   
 total.win <- sum(bets.df$amount[bets.df$horse == results[1]])
--- src/functions.R
+++ src/functions.R
@@ -3,21 +3,22 @@
 ## Include project-specific functions in this file
 
 FinalPayout <- function(bet.df, bet.type = "win", results){
-  bet.df <- bet.df[bet.df$type == bet.type, ]
-  kBets <- length(bet.df$horse)
+  new.df <- bet.df[bet.df$type == bet.type, ]
+  print(new.df)
+  kBets <- length(new.df$horse)
   bet.types <- 1:3
   names(bet.types) <- c("win", "place", "show")
   kFactor <- bet.types[bet.type]
-  odds <- FinalOdds(bet.df, kFactor = kFactor)
+  odds <- FinalOdds(new.df, kFactor = kFactor)
   win.payouts <- rep(0, kBets)
   for (i in 1:kBets) {
-    if(bet.df$horse[i] %in% results[1:kFactor]) {
-      multiplier <- odds$odds[odds$horses == bet.df$horse[i]]
-      win.payouts[i] <- bet.df$amount[i]*(multiplier + 1)
+    if(new.df$horse[i] %in% results[1:kFactor]) {
+      multiplier <- odds$odds[odds$horses == new.df$horse[i]]
+      win.payouts[i] <- new.df$amount[i]*(multiplier + 1)
     }
   }
-  bet.df$payouts <- win.payouts
-  return(bet.df)
+  new.df$payouts <- win.payouts
+  return(new.df)
 }
 
 FinalPayoutOld <- function(bet.df, bet.type = "win", results){
